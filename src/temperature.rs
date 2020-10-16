c4017afa137aad65a1dc2c4ec77c57d4cd7b47ea
knoby <maximilian.brinkmann@posteo.de>
2020-10-16 23:21:53 +0200

    Minor changes to production

--- src/temperature.rs
+++ src/temperature.rs
@@ -2,13 +2,13 @@ use crate::hal;
 use crate::onewire;
 use hal::prelude::*;
 
-const BUFFER_TOP_SENSOR_ADD: [u8; 8] = [40, 255, 123, 88, 85, 22, 3, 123];
+const WARM_WATER_SENSOR_ADD: [u8; 8] = [40, 255, 123, 88, 85, 22, 3, 123];
 const BUFFER_BUTTOM_SENSOR_ADD: [u8; 8] = [0x28, 0xFF, 0x2F, 0x96, 0x74, 0x16, 0x04, 0x61];
-const WARM_WATER_SENSOR_ADD: [u8; 8] = [0x28, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+const BUFFER_TOP_SENSOR_ADD: [u8; 8] = [0x28, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
 const BOILER_SENSOR_ADD: [u8; 8] = [40, 255, 65, 130, 88, 22, 4, 203];
 
-pub const MIN_BUFFER_TEMPERATURE: i16 = 25; // °C
-pub const BUFFER_HYSTERESIS: i16 = 2; // K
+pub const MIN_BUFFER_TEMPERATURE: i16 = 55; // °C
+pub const BUFFER_HYSTERESIS: i16 = 5; // K
 
 const _ALARM_TEMP_LOW: i8 = 5;
 const _ALARM_TEMP_HIGH: i8 = 95;
@@ -51,43 +51,6 @@ impl Sensors {
             buffer_top,
             buffer_buttom,
             boiler,
-        }
-    }
-
-    pub fn print_sensors(&mut self, serial: &mut dyn core::fmt::Write) {
-        // Create the search state
-        let mut search_state = onewire::SearchState::new();
-
-        serial
-            .write_str("Searchsing OneWire Bus for devices\n")
-            .ok();
-
-        loop {
-            match self.bus.search(
-                &mut search_state,
-                &mut hal::delay::Delay::<crate::Clock>::new(),
-            ) {
-                Ok(Some(rom_no)) => {
-                    serial.write_str("Found Device: ").ok();
-                    for byte in rom_no.iter() {
-                        //uwrite!(&mut serial, "{}", byte).ok();
-                        serial.write_char(' ').ok();
-                    }
-                    serial.write_char('\n').ok();
-                }
-                Ok(None) => {
-                    serial.write_str("Nothing found").ok();
-                    break;
-                }
-                Err(e) => {
-                    if e == onewire::Error::SearchEnd {
-                        serial.write_str("No more devices found\n").ok();
-                    } else {
-                        serial.write_str("An Error Occured\n").ok();
-                    };
-                    break;
-                }
-            }
         }
     }
 
